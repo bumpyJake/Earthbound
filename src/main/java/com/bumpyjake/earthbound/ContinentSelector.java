198ab3bc52066192576afbf2c2e765264d363ceb
Jake Woffinden <jakewoffinden@outlook.com>
2022-05-31 16:46:29 +0100

    GUI done 2

--- src/main/java/com/bumpyjake/earthbound/ContinentSelector.java
+++ src/main/java/com/bumpyjake/earthbound/ContinentSelector.java
@@ -6,13 +6,11 @@ import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
-import static sun.reflect.misc.FieldUtil.getField;
 
 public class ContinentSelector implements CommandExecutor {
 
