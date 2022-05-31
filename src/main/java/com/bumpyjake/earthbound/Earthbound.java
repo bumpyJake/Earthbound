c2a331b6d994c2fb565c858bf05517104663d9a3
Jake Woffinden <jakewoffinden@outlook.com>
2022-05-31 15:59:02 +0100

    Initial commit

---
+++ src/main/java/com/bumpyjake/earthbound/Earthbound.java
@@ -0,0 +1,17 @@
+package com.bumpyjake.earthbound;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Earthbound extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
