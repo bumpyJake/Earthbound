42c0bc3ab3ca8c997828672bc0905847fb8b6a84
Jake Woffinden <jakewoffinden@outlook.com>
2022-06-01 23:00:07 +0100

    Live command

--- src/main/java/com/bumpyjake/earthbound/SetContinentSpawns.java
+++ src/main/java/com/bumpyjake/earthbound/SetContinentSpawns.java
@@ -18,91 +18,93 @@ public class SetContinentSpawns implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 
-        if (args.length == 0){
-            sender.sendMessage("You must include a continent");
-        }else if (args.length == 1){
+        Player player = (Player) sender;
 
-            if(args[0].equalsIgnoreCase("northamerica")) {
-                if (sender instanceof Player){
-                    Player player = (Player) sender;
-                    Location location = player.getLocation();
-                    plugin.getConfig().set("NorthAmerica", location);
-                    plugin.saveConfig();
-                    player.sendMessage(ChatColor.GREEN + "North America Set");
 
+        if (player.hasPermission("earthbound.setcont")){
+            if (args.length == 0){
+                sender.sendMessage("You must include a continent");
+            }else if (args.length == 1){
+
+                if(args[0].equalsIgnoreCase("northamerica")) {
+                    if (sender instanceof Player){
+                        Location location = player.getLocation();
+                        plugin.getConfig().set("NorthAmerica", location);
+                        plugin.saveConfig();
+                        player.sendMessage(ChatColor.GREEN + "North America Set");
+
+                    }else{
+                        System.out.println("This command can only be ran by a player.");
+                    }
+                } else if(args[0].equalsIgnoreCase("europe")) {
+                    if (sender instanceof Player){
+                        Location location = player.getLocation();
+                        plugin.getConfig().set("Europe", location);
+                        plugin.saveConfig();
+                        player.sendMessage(ChatColor.GREEN + "Europe Set");
+
+                    }else{
+                        System.out.println("This command can only be ran by a player.");
+                    }
+                }else if(args[0].equalsIgnoreCase("oceania")) {
+                    if (sender instanceof Player){
+                        Location location = player.getLocation();
+                        plugin.getConfig().set("Oceania", location);
+                        plugin.saveConfig();
+                        player.sendMessage(ChatColor.GREEN + "Oceania Set");
+
+                    }else{
+                        System.out.println("This command can only be ran by a player.");
+                    }
+                }else if(args[0].equalsIgnoreCase("antartica")) {
+                    if (sender instanceof Player){
+                        Location location = player.getLocation();
+                        plugin.getConfig().set("Anartica", location);
+                        plugin.saveConfig();
+                        player.sendMessage(ChatColor.GREEN + "Anartica Set");
+
+                    }else{
+                        System.out.println("This command can only be ran by a player.");
+                    }
+                }else if(args[0].equalsIgnoreCase("asia")) {
+                    if (sender instanceof Player){
+                        Location location = player.getLocation();
+                        plugin.getConfig().set("Asia", location);
+                        plugin.saveConfig();
+                        player.sendMessage(ChatColor.GREEN + "Asia Set");
+
+                    }else{
+                        System.out.println("This command can only be ran by a player.");
+                    }
+                }else if(args[0].equalsIgnoreCase("southamerica")) {
+                    if (sender instanceof Player){
+                        Location location = player.getLocation();
+                        plugin.getConfig().set("SouthAmerica", location);
+                        plugin.saveConfig();
+                        player.sendMessage(ChatColor.GREEN + "South America Set");
+
+                    }else{
+                        System.out.println("This command can only be ran by a player.");
+                    }
+                }else if(args[0].equalsIgnoreCase("africa")) {
+                    if (sender instanceof Player){
+                        Location location = player.getLocation();
+                        plugin.getConfig().set("Africa", location);
+                        plugin.saveConfig();
+                        player.sendMessage(ChatColor.GREEN + "Africa Set");
+
+                    }else{
+                        System.out.println("This command can only be ran by a player.");
+                    }
                 }else{
-                    System.out.println("This command can only be ran by a player.");
+                    sender.sendMessage("That is not an available continent");
                 }
-            } else if(args[0].equalsIgnoreCase("europe")) {
-                if (sender instanceof Player){
-                    Player player = (Player) sender;
-                    Location location = player.getLocation();
-                    plugin.getConfig().set("Europe", location);
-                    plugin.saveConfig();
-                    player.sendMessage(ChatColor.GREEN + "Europe Set");
-
-                }else{
-                    System.out.println("This command can only be ran by a player.");
-                }
-            }else if(args[0].equalsIgnoreCase("oceania")) {
-                if (sender instanceof Player){
-                    Player player = (Player) sender;
-                    Location location = player.getLocation();
-                    plugin.getConfig().set("Oceania", location);
-                    plugin.saveConfig();
-                    player.sendMessage(ChatColor.GREEN + "Oceania Set");
-
-                }else{
-                    System.out.println("This command can only be ran by a player.");
-                }
-            }else if(args[0].equalsIgnoreCase("antartica")) {
-                if (sender instanceof Player){
-                    Player player = (Player) sender;
-                    Location location = player.getLocation();
-                    plugin.getConfig().set("Anartica", location);
-                    plugin.saveConfig();
-                    player.sendMessage(ChatColor.GREEN + "Anartica Set");
-
-                }else{
-                    System.out.println("This command can only be ran by a player.");
-                }
-            }else if(args[0].equalsIgnoreCase("asia")) {
-                if (sender instanceof Player){
-                    Player player = (Player) sender;
-                    Location location = player.getLocation();
-                    plugin.getConfig().set("Asia", location);
-                    plugin.saveConfig();
-                    player.sendMessage(ChatColor.GREEN + "Asia Set");
-
-                }else{
-                    System.out.println("This command can only be ran by a player.");
-                }
-            }else if(args[0].equalsIgnoreCase("southamerica")) {
-                if (sender instanceof Player){
-                    Player player = (Player) sender;
-                    Location location = player.getLocation();
-                    plugin.getConfig().set("SouthAmerica", location);
-                    plugin.saveConfig();
-                    player.sendMessage(ChatColor.GREEN + "South America Set");
-
-                }else{
-                    System.out.println("This command can only be ran by a player.");
-                }
-            }else if(args[0].equalsIgnoreCase("africa")) {
-                if (sender instanceof Player){
-                    Player player = (Player) sender;
-                    Location location = player.getLocation();
-                    plugin.getConfig().set("Africa", location);
-                    plugin.saveConfig();
-                    player.sendMessage(ChatColor.GREEN + "Africa Set");
-
-                }else{
-                    System.out.println("This command can only be ran by a player.");
-                }
-            }else{
-                sender.sendMessage("That is not an available continent");
             }
+        }else{
+            player.sendMessage("You don't have permission.");
         }
+
+
 
 
 
